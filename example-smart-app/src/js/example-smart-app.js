be88aa9bae21bbf9f708bab1788ec2eef384ad20
Lilykin <ilitea.kina@polymtl.ca>
2025-04-07 16:44:14 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -32,9 +32,15 @@
           var lname = '';
 
           if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
+            fname = Array.isArray(patient.name[0].given)
+              ? patient.name[0].given.join(' ')
+              : patient.name[0].given;
+          
+            lname = Array.isArray(patient.name[0].family)
+              ? patient.name[0].family.join(' ')
+              : patient.name[0].family;
           }
+
 
           var height = byCodes('8302-2');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
